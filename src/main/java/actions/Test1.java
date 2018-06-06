27ee4d4e9621b274d2deab53760396d3c80365ec
sap7deb <sap7deb@gmail.com>
2018-06-07 00:09:00 +0530

    new1

---
+++ src/main/java/actions/Test1.java
@@ -0,0 +1,13 @@
+package main.java.actions;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	@Test
+	public void t1() {
+		assertTrue(true);
+	}
+}
